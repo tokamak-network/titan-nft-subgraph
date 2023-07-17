d33a77a3ad8308727000b0a7bc89cf89f1c809c0
justin_g <64007655+usgeeus@users.noreply.github.com>
2023-07-18 01:25:43 +0900

    titanNFT Contract Event Indexer

---
+++ src/mappings/titanNFT.ts
@@ -0,0 +1,56 @@
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
+import {
+  Transfer,
+  SetAttribute,
+  Approval as ApprovalEvent,
+  ApprovalForAll
+} from '../../generated/TitanNFT/TitanNFT';
+import {
+  NFT,
+  Approval,
+} from '../../generated/schema';
+
+export function handleTransfer(event: Transfer): void {
+  let nft = NFT.load(event.params.tokenId.toString());
+  if (!nft) {
+    nft = new NFT(event.params.tokenId.toString());
+    nft.ownerHistory = [];
+  }
+  nft.owner = event.params.to;
+  nft.tokenID = event.params.tokenId;
+  let ownerHistory = nft.ownerHistory;
+  ownerHistory!.push(event.params.to);
+  nft.ownerHistory = ownerHistory;
+  nft.save();
+}
+export function handleSetAttribute(event: SetAttribute): void {
+  let nft = NFT.load(event.params.tokenId.toString());
+  if (!nft) {
+    nft = new NFT(event.params.tokenId.toString());
+    nft.ownerHistory = [];
+  }
+  nft.attribute = event.params.attribute
+  nft.save();
+}
+export function handleApproval(event: ApprovalEvent): void {
+  let approval = Approval.load(event.params.owner.toHexString() + event.params.approved.toHexString());
+  if (!approval) {
+    approval = new Approval(event.params.owner.toHexString() + event.params.approved.toHexString());
+  }
+  approval.from = event.params.owner;
+  approval.to = event.params.approved;
+  approval.tokenID = event.params.tokenId;
+  approval.forAll = false;
+  approval.save();
+}
+export function handleApprovalforAll(event: ApprovalForAll): void {
+  let approval = Approval.load(event.params.owner.toHexString() + event.params.operator.toHexString());
+  if (!approval) {
+    approval = new Approval(event.params.owner.toHexString() + event.params.operator.toHexString());
+  }
+  approval.from = event.params.owner;
+  approval.to = event.params.operator;
+  approval.tokenID = BigInt.fromI32(-1);
+  approval.forAll = true;
+  approval.save();
+}
